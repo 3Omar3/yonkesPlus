c2b179d3df5e231f2c9959c897c49ef8042d8c66
Emmanuel Omar <omarnuel123@gmail.com>
2021-02-07 02:25:32 -0600

    navbar added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 const Index = () => {
   return (
@@ -8,21 +9,98 @@ const Index = () => {
       <Head>
         <title>YonkesPlus | Inicio</title>
       </Head>
-      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-        <nav
-          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
-          aria-label="Global"
-        >
-          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-            <div className="flex items-center justify-between w-full md:w-auto">
-              <div className="-mr-2 flex items-center md:hidden">
+
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <nav
+            className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+            aria-label="Global"
+          >
+            <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+              <div className="flex items-center justify-between w-full md:w-auto">
+                <Link href="#">
+                  <a>
+                    <Image src="/logo.svg" height={50} width={195} alt="logo" />
+                  </a>
+                </Link>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <button
+                    type="button"
+                    className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    id="main-menu"
+                    aria-haspopup="true"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+              <a
+                href="#"
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                Categorías
+              </a>
+
+              <a
+                href="#"
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                Vehículo
+              </a>
+
+              <a
+                href="#"
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                Mis compras
+              </a>
+
+              <a
+                href="#"
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                Carrito
+              </a>
+
+              <a
+                href="#"
+                className="font-medium text-blue-600 hover:text-blue-500"
+              >
+                Ingresar
+              </a>
+            </div>
+          </nav>
+        </div>
+
+        <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div className="h-8 w-auto">
+                <Image height={45} width={160} src="/logo.svg" alt="" />
+              </div>
+              <div className="-mr-2">
                 <button
                   type="button"
                   className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                  id="main-menu"
-                  aria-haspopup="true"
                 >
-                  <span className="sr-only">Open main menu</span>
+                  <span className="sr-only">Close main menu</span>
                   <svg
                     className="h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -35,71 +113,69 @@ const Index = () => {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
+                      d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
                 </button>
               </div>
             </div>
-          </div>
-          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-            <Link href="#">
-              <a className="font-medium text-gray-500 hover:text-gray-900">
-                Product
-              </a>
-            </Link>
-            <Link href="#">
-              <a className="font-medium text-gray-500 hover:text-gray-900">
-                Features
-              </a>
-            </Link>
-            <Link href="#">
-              <a className="font-medium text-gray-500 hover:text-gray-900">
-                Marketplace
-              </a>
-            </Link>
-            <Link href="#">
-              <a className="font-medium text-gray-500 hover:text-gray-900">
-                Company
-              </a>
-            </Link>
-            <Link href="#">
-              <a className="font-medium text-indigo-600 hover:text-indigo-500"></a>
-            </Link>
-          </div>
-        </nav>
-      </div>
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Bienvenido a </span>
-            <span className="block text-indigo-600 xl:inline">Yonkes+</span>
-          </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
-          </p>
-          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div className="rounded-md shadow">
-              <a
-                href="#"
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-              >
-                Get started
-              </a>
-            </div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">
-              <a
-                href="#"
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-              >
-                Live demo
-              </a>
+            <div
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="main-menu"
+            >
+              <div className="px-2 pt-2 pb-3 space-y-1" role="none">
+                <Link href="#">
+                  <a
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Categorías
+                  </a>
+                </Link>
+                <Link href="#">
+                  <a
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Vehículo
+                  </a>
+                </Link>
+                <Link href="#">
+                  <a
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Mis compras
+                  </a>
+                </Link>
+                <Link href="#">
+                  <a
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Carrito
+                  </a>
+                </Link>
+              </div>
+              <div role="none">
+                <Link href="#">
+                  <a
+                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Ingresar
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </main>
+
+        <main className="mt-10 mx-auto max-w-1xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-5 lg:px-8 xl:mt-10 tracking-wide">
+          <p>Hello world this is the content of the page</p>
+        </main>
+      </div>
     </div>
   );
 };
